f34eedb6c2ac5279f7dec0072accf43bb4245993
aalbareedi <aalbareedi@yahoo.com>
2020-04-07 06:06:39 -0500

    navToggleBtn functionality

--- js/domObjects.js
+++ js/domObjects.js
@@ -5,8 +5,9 @@ let scrollingElement =
 // let backToTopBtn = document.getElementById("backToTopBtn");
 let backToTopBtn = document.querySelector(".backToTopBtn");
 let navToggleBtn = document.querySelector(".navToggleBtn");
-
-// let navLogo = document.querySelector(".navLogo");
+let navLogoBar = document.querySelector(".navLogoBar");
+let navToggleIcon = document.querySelector(".navToggleIcon");
+let navMenu = document.querySelector(".navMenu");
 // let navLogoImg = document.querySelector(".navLogoImg");
 // let logoTextTop = document.querySelector(".logoTextTop");
 // let logoTextBottom = document.querySelector(".logoTextBottom");
--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -1,6 +1,20 @@
 // Remove Gray Highlight When Tapping Links in Mobile Safari
 document.addEventListener("touchstart", function () {}, true);
 
+navToggleBtn.onclick = function () {
+  if (navLogoBar.classList.contains("openNavLogoBar") == false) {
+    navLogoBar.classList.add("openNavLogoBar");
+    navToggleIcon.classList.add("is-active");
+    navMenu.classList.add("openNavMenu");
+    // openNavMenu();
+  } else {
+    // closeNavMenu();
+    navLogoBar.classList.remove("openNavLogoBar");
+    navToggleIcon.classList.remove("is-active");
+    navMenu.classList.remove("openNavMenu");
+  }
+};
+
 // body.onscroll = function () {
 //   if (scrollingElement.scrollTop > 0) {
 //     backToTopBtn.classList.remove("opacityZero");
