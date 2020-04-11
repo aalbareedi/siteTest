22b3fa439c00c9378e90db69b4ecb9129f74d4fb
aalbareedi <aalbareedi@yahoo.com>
2020-04-10 20:01:03 -0500

    typewrtier effect

--- js/functions.js
+++ js/functions.js
@@ -12,16 +12,37 @@
 //   navMenu.classList.remove("openNavMenu");
 // }
 
-function navigateToPage(page) {
-  navToggleIcon.classList.remove("is-active");
-  navMenu.classList.remove("openNavMenu");
-  aboutBox.classList.remove("openBox");
-  portfolioBox.classList.remove("openBox");
-  contactBox.classList.remove("openBox");
-  landing.classList.remove("openBox");
-  page.classList.add("openBox");
-}
-
-// function nextWord() {
-//   siteTextWords[index];
+// function navigateToPage(page) {
+//   navToggleIcon.classList.remove("is-active");
+//   navMenu.classList.remove("openNavMenu");
+//   aboutBox.classList.remove("openBox");
+//   portfolioBox.classList.remove("openBox");
+//   contactBox.classList.remove("openBox");
+//   landing.classList.remove("openBox");
+//   page.classList.add("openBox");
 // }
+
+function nextWord() {
+  let siteTextWords = [
+    "Hi!",
+    "My",
+    "name",
+    "is",
+    `<span class="myNameValue">Amer Albareedi</span>`,
+    "and",
+    "I",
+    "am",
+    "a",
+    `<span class="jobTitleValue">
+    Front-End Developer</span>.`,
+  ];
+  let index = 0;
+
+  let interval = setInterval(function () {
+    introBox.innerHTML = introBox.innerHTML + " " + siteTextWords[index];
+    index++;
+    if (index >= siteTextWords.length) {
+      clearInterval(interval);
+    }
+  }, 300);
+}
--- js/script.js
+++ js/script.js
@@ -4,23 +4,10 @@
 //   alert(window.innerWidth + "," + window.innerHeight);
 // }, 1000);
 
-let siteText = `Hi! My name is <span class="myNameValue">Amer Albareedi</span>
-and I am a
-<span class="jobTitleValue">
-  Front-End Developer</span>.
-`;
+// let siteText = `Hi! My name is <span class="myNameValue">Amer Albareedi</span>
+// and I am a
+// <span class="jobTitleValue">
+//   Front-End Developer</span>.
+// `;
 
-let siteTextWords = [
-  "Hi!",
-  "My",
-  "name",
-  "is",
-  `<span class="myNameValue">Amer Albareedi</span>`,
-  "and",
-  "I",
-  "am",
-  "a",
-  `<span class="jobTitleValue">
-  Front-End Developer</span>.`,
-];
-let index = 0;
+// nextWord();
