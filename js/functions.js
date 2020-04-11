578347f96de1abf71a6c3661f1f253dde1f6b81c
aalbareedi <aalbareedi@yahoo.com>
2020-04-11 00:31:01 -0500

    typewriter

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,17 @@
 // function openNavMenu() {
-//   navWindow.classList.add("openNavWindow");
-//   navLogoBar.classList.add("openNavLogoBar");
-//   navToggleIcon.classList.add("is-active");
-//   navMenu.classList.add("openNavMenu");
 // }
-
 // function closeNavMenu() {
-//   navWindow.classList.remove("openNavWindow");
-//   navLogoBar.classList.remove("openNavLogoBar");
-//   navToggleIcon.classList.remove("is-active");
-//   navMenu.classList.remove("openNavMenu");
 // }
 
-// function navigateToPage(page) {
-//   navToggleIcon.classList.remove("is-active");
-//   navMenu.classList.remove("openNavMenu");
-//   aboutBox.classList.remove("openBox");
-//   portfolioBox.classList.remove("openBox");
-//   contactBox.classList.remove("openBox");
-//   landing.classList.remove("openBox");
-//   page.classList.add("openBox");
-// }
+function navigateToPage(page) {
+  navToggleIcon.classList.remove("is-active");
+  navMenu.classList.remove("openNavMenu");
+  aboutBox.classList.remove("openBox");
+  portfolioBox.classList.remove("openBox");
+  contactBox.classList.remove("openBox");
+  landing.classList.remove("openBox");
+  page.classList.add("openBox");
+}
 
 function nextWord() {
   let siteTextWords = [
