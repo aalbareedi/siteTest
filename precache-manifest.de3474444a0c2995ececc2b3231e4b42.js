c0f4a622ae4feb9150120bed023c9f86c50d60ac
aalbareedi <aalbareedi@yahoo.com>
2020-07-09 12:58:19 -0500

    update

---
+++ precache-manifest.de3474444a0c2995ececc2b3231e4b42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10060edaefc8a4548f5c00978276f7b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5de777757a8887f9297c",
+    "url": "./static/css/main.5bf69a7f.chunk.css"
+  },
+  {
+    "revision": "9be1274374630e615870",
+    "url": "./static/js/2.51285f97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.51285f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de777757a8887f9297c",
+    "url": "./static/js/main.5d57b74c.chunk.js"
+  },
+  {
+    "revision": "dd5dc5f19c8b588c5d5c",
+    "url": "./static/js/runtime-main.45bcaf70.js"
+  },
+  {
+    "revision": "504016b920d5cf058d438e4e2fc8b419",
+    "url": "./static/media/newcheck.504016b9.svg"
+  }
+]);
