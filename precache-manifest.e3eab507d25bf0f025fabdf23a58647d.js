586298e6575fde67f2ee9755c4a51fffd3dda807
aalbareedi <aalbareedi@yahoo.com>
2020-08-24 03:03:07 -0500

    ttesting

---
+++ precache-manifest.e3eab507d25bf0f025fabdf23a58647d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52678e86cb6eae1f0ac4e104b0586742",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18fd13f7ddaff60cc312",
+    "url": "./static/css/main.09349592.chunk.css"
+  },
+  {
+    "revision": "9be1274374630e615870",
+    "url": "./static/js/2.51285f97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.51285f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18fd13f7ddaff60cc312",
+    "url": "./static/js/main.902f0c7e.chunk.js"
+  },
+  {
+    "revision": "dd5dc5f19c8b588c5d5c",
+    "url": "./static/js/runtime-main.45bcaf70.js"
+  },
+  {
+    "revision": "504016b920d5cf058d438e4e2fc8b419",
+    "url": "./static/media/newcheck.504016b9.svg"
+  },
+  {
+    "revision": "710eeadef93b1c1fc170346bb7a9005b",
+    "url": "./static/media/smilebg1.710eeade.png"
+  }
+]);
