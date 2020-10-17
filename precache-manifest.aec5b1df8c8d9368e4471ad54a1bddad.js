20d798642364e6a04d54e00d7b68004df161d0e8
aalbareedi <aalbareedi@yahoo.com>
2020-10-17 18:32:15 -0500

    new favicon test

---
+++ precache-manifest.aec5b1df8c8d9368e4471ad54a1bddad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149dcfcd554da46dd776c2c38ca017da",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be8904eefddb6b9714f6",
+    "url": "./static/css/2.34c0de18.chunk.css"
+  },
+  {
+    "revision": "00190d6c05d906308246",
+    "url": "./static/css/main.c6984765.chunk.css"
+  },
+  {
+    "revision": "be8904eefddb6b9714f6",
+    "url": "./static/js/2.ff4b9533.chunk.js"
+  },
+  {
+    "revision": "210e5fea5d657e0a340abfa54200e342",
+    "url": "./static/js/2.ff4b9533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00190d6c05d906308246",
+    "url": "./static/js/main.eb788dfa.chunk.js"
+  },
+  {
+    "revision": "c2867b566b80a31492a4",
+    "url": "./static/js/runtime-main.5654d317.js"
+  },
+  {
+    "revision": "504016b920d5cf058d438e4e2fc8b419",
+    "url": "./static/media/newcheck.504016b9.svg"
+  },
+  {
+    "revision": "710eeadef93b1c1fc170346bb7a9005b",
+    "url": "./static/media/smilebg1.710eeade.png"
+  }
+]);
