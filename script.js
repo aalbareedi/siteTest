faa2c1485007462d626eda19ac0f95385b0fc437
aalbareedi <aalbareedi@yahoo.com>
2021-05-12 02:56:02 -0500

    site functionality test

---
+++ script.js
@@ -0,0 +1,53 @@
+let openNavBtn = document.querySelector(".openNavBtn");
+let closeNavBtn = document.querySelector(".closeNavBtn");
+let navWindow = document.querySelector("nav");
+let ctaBtn = document.querySelector(".ctaBtn");
+let ctaBtnText = document.querySelector(".ctaBtnText");
+let form = document.querySelector("form");
+let closeFormBtn = document.querySelector(".closeFormBtn");
+let backToTop = document.querySelector(".backToTop");
+let body = document.querySelector("body");
+
+// openNavBtn.onclick = () => {
+//   navWindow.classList.remove("displayHidden");
+// };
+
+// closeNavBtn.onclick = () => {
+//   navWindow.classList.add("displayHidden");
+// };
+
+openNavBtn.onclick = () => {
+  navWindow.classList.add("showNav");
+};
+
+closeNavBtn.onclick = () => {
+  navWindow.classList.remove("showNav");
+};
+
+ctaBtn.onclick = () => {
+  form.classList.add("showForm");
+};
+
+closeFormBtn.onclick = () => {
+  form.classList.remove("showForm");
+};
+
+backToTop.onclick = () => {
+  body.scrollIntoView(true);
+};
+
+// we get the buttons initial position outside the scroll event
+// so it doesnt change when we add the class to it
+let ctaBtnPosition = ctaBtn.offsetTop;
+
+body.onscroll = () => {
+  let bodyScroll = document.documentElement.scrollTop || body.scrollTop;
+
+  if (bodyScroll > ctaBtnPosition) {
+    ctaBtn.classList.add("btnScrolled");
+  } else {
+    ctaBtn.classList.remove("btnScrolled");
+  }
+
+  console.log(ctaBtnPosition);
+};
