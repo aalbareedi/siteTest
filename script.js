908538c88b9e1e5a19106dfe1da9ef7164fd86b8
aalbareedi <aalbareedi@yahoo.com>
2021-06-24 02:11:16 -0500

    parallax test

--- script.js
+++ script.js
@@ -19,8 +19,10 @@ let box2 = document.querySelector(".box2");
 let box3 = document.querySelector(".box3");
 let box4 = document.querySelector(".box4");
 let box5 = document.querySelector(".box5");
+let box6 = document.querySelector(".box6");
 let boxes = document.querySelectorAll(".box");
 let landingBgImg = document.querySelector(".landingBgImg");
+let box5parallaxImg = document.querySelector(".box5parallaxImg");
 let desktopNav = document.querySelector(".desktopNav");
 let desktopNavLink1 = document.querySelector("#desktopNavLink1");
 let desktopNavLink2 = document.querySelector("#desktopNavLink2");
@@ -31,6 +33,9 @@ let navSublinks = document.querySelectorAll(
   // selects all elements that have href = #
   ".navSublink[href='#'], .desktopNavLink[href='#']"
 );
+let toggleText = document.querySelector(".toggleText");
+let moreTextBtn = document.querySelector(".moreTextBtn");
+let lessTextBtn = document.querySelector(".lessTextBtn");
 
 // openNavBtn.onclick = () => {
 //   navWindow.classList.remove("displayHidden");
@@ -88,10 +93,17 @@ body.onscroll = () => {
     ctaBtn.classList.remove("btnScrolled");
   }
 
+  // hiding parallax imgs to avoid appearing after scroll.
   if (box2.getBoundingClientRect().top < 0) {
     landingBgImg.classList.add("displayHidden");
   } else {
     landingBgImg.classList.remove("displayHidden");
+  }
+
+  if (box6.getBoundingClientRect().top < 0) {
+    box5parallaxImg.classList.add("displayHidden");
+  } else {
+    box5parallaxImg.classList.remove("displayHidden");
   }
 
   // console.log(ctaBtnPosition);
@@ -150,6 +162,7 @@ desktopNav.onclick = (e) => {
   e.stopPropagation();
 };
 
+// scrolling to desktopNavLinks
 for (let i = 0; i < desktopNavLinks.length; i++) {
   //console.log(desktopNavLinks[i].href);
 
@@ -200,3 +213,15 @@ const swiper = new Swiper(".swiper-container", {
   //   el: ".swiper-scrollbar",
   // },
 });
+
+moreTextBtn.onclick = () => {
+  toggleText.classList.remove("lessToggleText");
+  moreTextBtn.classList.add("displayHidden");
+  lessTextBtn.classList.remove("displayHidden");
+};
+
+lessTextBtn.onclick = () => {
+  toggleText.classList.add("lessToggleText");
+  moreTextBtn.classList.remove("displayHidden");
+  lessTextBtn.classList.add("displayHidden");
+};
