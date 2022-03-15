82582327878eac7bcbebab1319816e6f8a6406d4
aalbareedi <aalbareedi@yahoo.com>
2022-03-14 23:27:20 -0500

    a

--- script.js
+++ script.js
@@ -31,3 +31,9 @@ setTimeout(function () {
   pageSize.innerHTML = window.innerWidth + ", " + window.innerHeight;
   pageSize.classList.remove("displayHidden");
 }, 3000);
+
+window.addEventListener("resize", () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
