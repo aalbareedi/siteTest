69fd0914ae161b93676082f4cf7df0a4c163873a
aalbareedi <38078785+aalbareedi@users.noreply.github.com>
2022-11-10 12:32:44 -0600

    latest BASE version!

--- phpmailer/form.js
+++ phpmailer/form.js
@@ -1,62 +1,52 @@
+jQuery(function () {
+  function after_form_submitted(response) {
+    const data = response.responseJSON || {};
 
-jQuery(function()
-{
-    function after_form_submitted(data) 
-    {
-        if(data.result == 'success')
-        {
-            jQuery('form#reused_form').hide();
-            jQuery('#success_message').show();
-            jQuery('#error_message').hide();
+    if (data.result == "success") {
+      jQuery("form#message-form").trigger("reset");
+      jQuery("#success_message").show();
+      jQuery("#error_message").hide();
+    } else {
+      jQuery("#error_message").append("<ul></ul>");
+
+      jQuery.each(response.errors, function (key, val) {
+        jQuery("#error_message ul").append("<li>" + key + ":" + val + "</li>");
+      });
+      jQuery("#success_message").hide();
+      jQuery("#error_message").show();
+
+      //reverse the response on the button
+      jQuery('button[type="button"]', $form).each(function () {
+        $btn = jQuery(this);
+        label = $btn.prop("orig_label");
+        if (label) {
+          $btn.prop("type", "submit");
+          $btn.text(label);
+          $btn.prop("orig_label", "");
         }
-        else
-        {
-            jQuery('#error_message').append('<ul></ul>');
+      });
+    } //else
+  }
 
-            jQuery.each(data.errors,function(key,val)
-            {
-             jQuery('#error_message ul').append('<li>'+key+':'+val+'</li>');
-            });
-            jQuery('#success_message').hide();
-            jQuery('#error_message').show();
+  jQuery("#message-form").submit(function (e) {
+    e.preventDefault();
 
-            //reverse the response on the button
-            jQuery('button[type="button"]', $form).each(function()
-            {
-                $btn = jQuery(this);
-                label = $btn.prop('orig_label');
-                if(label)
-                {
-                    $btn.prop('type','submit' ); 
-                    $btn.text(label);
-                    $btn.prop('orig_label','');
-                }
-            });
-            
-        }//else
-    }
+    $form = jQuery(this);
+    //show some response on the button
+    jQuery('button[type="submit"]', $form).each(function () {
+      $btn = jQuery(this);
+      $btn.prop("type", "button");
+      $btn.prop("orig_label", $btn.text());
+      $btn.text("Sending ...");
+    });
 
-	jQuery('#reused_form').submit(function(e)
-      {
-        e.preventDefault();
+    jQuery.ajax({
+      type: "POST",
+      url: "../../phpmailer/handler.php",
+      data: $form.serialize(),
+      complete: after_form_submitted,
 
-        $form = jQuery(this);
-        //show some response on the button
-        jQuery('button[type="submit"]', $form).each(function()
-        {
-            $btn = jQuery(this);
-            $btn.prop('type','button' ); 
-            $btn.prop('orig_label',$btn.text());
-            $btn.text('Sending ...');
-        });
-        
-               jQuery.ajax({
-                type: "POST",
-                url: '../../phpmailer/handler.php',
-                data: $form.serialize(),
-                success: after_form_submitted,
-                dataType: 'json' 
-            });        
-        
-      });	
+      dataType: "json",
+    });
+  });
 });
--- phpmailer/handler.php
+++ phpmailer/handler.php
@@ -1,27 +1,39 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+// /*
+// Tested working with PHP5.4 and above (including PHP 7 )
 
- */
-require_once './vendor/autoload.php';
+//  */
+// require_once './vendor/autoload.php';
 
-use FormGuide\Handlx\FormHandler;
-
-
-$pp = new FormHandler(); 
-
-$validator = $pp->getValidator();
-$validator->fields(['Name','Email'])->areRequired()->maxLength(50);
-$validator->fields(['Subject'])->maxLength(50);
-$validator->field('Email')->isEmail();
-$validator->field('Message')->maxLength(6000);
+// use FormGuide\Handlx\FormHandler;
 
 
+// $pp = new FormHandler(); 
+
+// $validator = $pp->getValidator();
+// $validator->fields(['Name','Email'])->areRequired()->maxLength(50);
+// $validator->fields(['Subject'])->maxLength(50);
+// $validator->field('Email')->isEmail();
+// $validator->field('Message')->maxLength(6000);
 
 
-$pp->sendEmailTo('someone@gmail.com'); // ← Your email here
 
-echo $pp->process($_POST);
+
+// $pp->sendEmailTo('aalbareedi@yahoo.com'); // ← Your email here
+
+// echo $pp->process($_POST);
+
+$body = "<b>Name: </b>" . htmlspecialchars($_POST["Name"]) . 
+"<br>" . "<b>Email: </b>" . htmlspecialchars($_POST["Email"]) . 
+"<br>" . "<b>Phone: </b>" . htmlspecialchars($_POST["Phone"]) . 
+"<br>" . "<b>Message: </b>" . htmlspecialchars($_POST["Message"]);
+
+$headers = "MIME-Version:1.0\r\n" . 
+"Content-type:text/html:charset=UTF-8\r\n" . 
+"Reply-To:" . $_POST["Email"] . "\r\n" . 
+"From: Braces Specialist<vpnfg0lsvg6z@a2plcpnl0239.prod.iad2.secureserver.net>";
+
+mail("aalbareedi@yahoo.com", "Message from " . $_POST["Name"], $body, $headers);
