62d3c2f7bb9fbc587bd8f1063aa3922080e4f9be
aalbareedi <38078785+aalbareedi@users.noreply.github.com>
2022-12-09 02:51:56 -0600

    project window

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const sidebar = document.querySelector("[data-sidebar]");
 menuIconButton.addEventListener("click", () => {
   sidebar.classList.toggle("open");
 });
+
+const projectWindow = document.querySelector(".project-window");
+const moreInfoBtn = document.querySelector(".more-info-btn");
+const closeProjectBtn = document.querySelector(".close-project-btn");
+moreInfoBtn.addEventListener("click", () => {
+  projectWindow.classList.toggle("hidden");
+});
+closeProjectBtn.addEventListener("click", () => {
+  projectWindow.classList.toggle("hidden");
+});
