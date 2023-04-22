68680d2b868d4f2f3a26efebf0485df3c491d6ea
aalbareedi <38078785+aalbareedi@users.noreply.github.com>
2023-04-21 22:03:56 -0500

    added react portfolio

---
+++ react-portfolio/precache-manifest.af25aa0e000c94d660750ab6cfaae627.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5ec995e4d36a2087d01f947d7809a3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0573d5399d9d57298851",
+    "url": "./static/css/2.cebec226.chunk.css"
+  },
+  {
+    "revision": "e58676777203a2978990",
+    "url": "./static/css/main.71270db0.chunk.css"
+  },
+  {
+    "revision": "0573d5399d9d57298851",
+    "url": "./static/js/2.b5c19335.chunk.js"
+  },
+  {
+    "revision": "65089b70899301b7933aa2160bb4101d",
+    "url": "./static/js/2.b5c19335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e58676777203a2978990",
+    "url": "./static/js/main.1503682a.chunk.js"
+  },
+  {
+    "revision": "c2867b566b80a31492a4",
+    "url": "./static/js/runtime-main.5654d317.js"
+  },
+  {
+    "revision": "ab2a112a9b40335a18ed5219fde68619",
+    "url": "./static/media/after1.ab2a112a.png"
+  },
+  {
+    "revision": "76683d450f1486d95989028a58a2de01",
+    "url": "./static/media/before2.76683d45.png"
+  },
+  {
+    "revision": "504016b920d5cf058d438e4e2fc8b419",
+    "url": "./static/media/newcheck.504016b9.svg"
+  },
+  {
+    "revision": "710eeadef93b1c1fc170346bb7a9005b",
+    "url": "./static/media/smilebg1.710eeade.png"
+  }
+]);
