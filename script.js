2bb655f56946eb7a54490dbcdd88c3f1d234461f
aalbareedi <38078785+aalbareedi@users.noreply.github.com>
2023-09-24 03:51:30 -0500

    nav settings window

---
+++ script.js
@@ -0,0 +1,8 @@
+let navBtn = document.querySelector(".nav-menu-btn");
+let settings = document.querySelector(".settings-window");
+
+navBtn.onclick = () => {
+  settings.classList.add("showSettings");
+};
+
+console.log("test");
