95ba1555d9017a493fda6b4c0709586b6b438f98
aalbareedi <38078785+aalbareedi@users.noreply.github.com>
2023-09-29 21:33:50 -0500

    table html added

--- components/crypto-entry.js
+++ components/crypto-entry.js
@@ -17,35 +17,39 @@ import { formatUsd, formatPercent } from "../utils/data-formatter.js";
 
 export default function CryptoEntry(number, { symbol, name, quote }, metadata) {
     // Return a new element/DOM object
-    const element = document.createElement("div");
+    const element = document.createElement("tr");
     element.classList.add("crypto-entry");
 
     element.innerHTML += `
-        <div class="crypto-entry-pair">
-            <div class="crypto-number">${number}</div>
+        <td class="crypto-number">${number}</td>
+        <td>
             <div class="crypto-name-wrapper">
-            <div class="crypto-logo">
-                <img src="${metadata ? metadata.logo : ""}" />
-            </div>
-            <div class="crypto-name">
-                <div class="crypto-symbol">${symbol}</div>
-                <div class="crypto-name-text display-hidden">${name}</div>
-                <div class="crypto-name-text">
-                    ${formatUsd(quote.USD.price)}
+                <div class="crypto-logo">
+                    <img src="${metadata ? metadata.logo : ""}" />
+                </div>
+                <div class="crypto-name">
+                    <div class="crypto-symbol">${symbol}</div>
+                    <div class="crypto-name-text display-hidden">${name}</div>
+                    <div class="crypto-name-text">
+                        ${formatUsd(quote.USD.price)}
+                    </div>
                 </div>
             </div>
+        </td>
+        <td>
+            <div
+                class="crypto-entry-perc ${
+                    quote.USD.percent_change_24h >= 0
+                        ? "positive-entry"
+                        : "negative-entry"
+                }"
+            >
+                ${formatPercent(quote.USD.percent_change_24h)}
             </div>
-        </div>
-        <div class="crypto-entry-perc ${
-            quote.USD.percent_change_24h >= 0
-                ? "positive-entry"
-                : "negative-entry"
-        }">
-            ${formatPercent(quote.USD.percent_change_24h)}
-        </div>
-        <div class="crypto-mcap">
+        </td>
+        <td class="crypto-mcap">
             ${formatUsd(quote.USD.market_cap)}
-        </div>
+        </td>
     `;
 
     return element;
