ca64d94752b11d775f3f356809db3bf5bea73a5c
aalbareedi <38078785+aalbareedi@users.noreply.github.com>
2023-10-08 22:23:12 -0500

    global stats carousel id fixes

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,27 +1,3 @@
-// const carousels = document.querySelectorAll(".carousel");
-
-// carousels.forEach((carousel) => {
-//     const clone = carousel.cloneNode(true);
-//     carousel.after(clone);
-
-//     carousel.classList.add("animated");
-//     clone.classList.add("animated");
-// });
-
-// // NOTE: This assumes that you have one pause button for every carousel...
-
-// const pauseButtons = document.querySelectorAll(".pause-btn");
-
-// pauseButtons.forEach((pauseButton, i) => {
-//     pauseButton.onclick = (e) => {
-//         e.preventDefault();
-//         e.stopPropagation();
-//         carousels[i].classList.toggle("paused");
-//         carousels[i].nextElementSibling.classList.toggle("paused");
-//         pauseButton.classList.toggle("is-paused");
-//     };
-// });
-
 export function makeCarouselWork(carousel) {
     const clone = carousel.cloneNode(true);
     carousel.after(clone);
