7a3921d64b8f4e119dcf6363d15023035504d1c9
aalbareedi <38078785+aalbareedi@users.noreply.github.com>
2023-10-08 23:27:19 -0500

    skeleton row added to infinite scroll

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,6 @@
 export const USE_SANDBOX_DATA = false;
+export const DISABLE_INFINITE_SCROLLING = false;
+
 export const MAX_SIGNIFICANT_DIGITS = 4;
 export const DEFAULT_CRYPTO_TIMEFRAME = "24h";
 export const DEFAULT_CRYPTO_QUANTITY = "All Coins";
